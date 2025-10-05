6a64c3c5f8a36b6f83a02ef9954a500f45984d1b
akashpanda-01 <1999akashpanda@gmail.com>
2025-10-05 19:20:32 +0530

    React Practice Project

--- app.js
+++ app.js
@@ -12,35 +12,63 @@ import ReactDOM from "react-dom/client";
 //   "Hello React World"
 // );
 
-const container = React.createElement(
-  "div",
-  { id: "container" },
-  React.createElement("ul", {}, [
-    React.createElement("li", { key: "1" }, "About us"),
-    React.createElement("li", { key: "2" }, "Menu"),
-  ])
-);
+// const container = React.createElement(
+//   "div",
+//   { id: "container" },
+//   React.createElement("ul", {}, [
+//     React.createElement("li", { key: "1" }, "About us"),
+//     React.createElement("li", { key: "2" }, "Menu"),
+//   ])
+// );
 
-const heading = <h1 id="title" key="heading">Namaste React</h1>;
+// const heading = <h1 id="title" key="heading">Namaste React</h1>;
 
-const Header = function () {
-  return <h1 id="title" key="Header">World</h1>;
+// const header1 = function () {
+//   return <h1 id="title" key="header1">Hello Jiii</h1>;
+// };
+
+// const Header = function () {
+//   return <h1 id="title" key="Header">World</h1>;
+// };
+
+const Title = () => {
+  return (
+    <a href="/">
+      <img
+        id="logo"
+        alt="logo"
+        src="https://i.pinimg.com/736x/e6/17/f1/e617f1bfb9af4d9cf132cd3dec0da072.jpg"
+      />
+    </a>
+  );
 };
-const header1 = function () {
-  return <h1 id="title" key="header1">Hello Jiii</h1>;
+
+const Header = () => {
+  return (
+    <>
+      <div id="header">
+        <Title />
+        <div id="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
-// Composing Component
-let HeadingComponent = () => {
+let AppLayout = () => {
   return (
     <div>
-      {heading},
       <Header />
-      {header1()}
-      <h1>Hello World</h1>
+      <Foo
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render([container, heading, <HeadingComponent />]);
+root.render(<AppLayout />);
