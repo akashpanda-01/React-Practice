c71b4fdb726a5407f55bbce5c22639abba420ea7
Akash <1999akashpanda@gmail.com>
2025-10-27 23:21:11 +0530

    Date-27

--- React-Namaste-Practice/src/components/Header.jsx
+++ React-Namaste-Practice/src/components/Header.jsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Header.css";
 
 const Title = () => {
   return (
     <a href="/">
       <img
-        id="logo"
+        className="logo"
         alt="logo"
-        src="https://i.pinimg.com/736x/e6/17/f1/e617f1bfb9af4d9cf132cd3dec0da072.jpg"
+        src="../../assets/FoodVilla.png"
+        // src="https://i.pinimg.com/736x/e6/17/f1/e617f1bfb9af4d9cf132cd3dec0da072.jpg"
       />
     </a>
-  )
+  );
 };
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
-      <div id="header">
+      <div className="header">
         <Title />
-        <div id="nav-items">
+        <div className="nav-items">
           <ul>
             <li>Home</li>
             <li>About</li>
@@ -25,10 +28,20 @@ const Header = () => {
             <li>Cart</li>
           </ul>
         </div>
+        <div>
+          {isLoggedIn ? (
+            <button onClick={() => setIsLoggedIn(false)} className="btn-logged">
+              Login
+            </button>
+          ) : (
+            <button onClick={() => setIsLoggedIn(true)} className="btn-logged">
+              Logout
+            </button>
+          )}
+        </div>
       </div>
     </>
   );
 };
-
 
 export default Header;
--- React-Namaste-Practice/src/components/RestaurantCard.jsx
+++ React-Namaste-Practice/src/components/RestaurantCard.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { IMG_CDN_URL } from "../constants";
-
+import "./RestaurantCard.css";
 
 const RestaurantCard = ({ restaurant }) => {
+  // console.log(restaurant)
   return (
     <>
       <div id="card">
         <img
           // src={`https://media-assets.swiggy.com/swiggy/image/upload/${props.resturant.data.cloudinaryImageId}`}
-          src={ IMG_CDN_URL + restaurant.card.data.cloudinaryImageId}
+          src={ IMG_CDN_URL + restaurant?.cloudinaryImageId}
+          alt={restaurant?.name}
         />
         {/* <h3>{props.resturant.data.name}</h3>
         <h4>{props.resturant.data.cuisines}</h4>
         <h5>{props.resturant.data.avgRating} Stars</h5> */}
-        <h3>{restaurant.card.data.name}</h3>
-        <h4>{restaurant.card.data.cuisines}</h4>
-        <h5>{restaurant.card.data.avgRating} Stars</h5>
+        <h3>{restaurant.name}</h3>
+        <h4>{restaurant.cuisines?.join(", ")}</h4>
+        <h5>{restaurant.avgRating} Stars</h5>
       </div>
     </>
   );
