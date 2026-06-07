5e880e78011ce9f249174a13f43a0109d5591d61
akashpanda-01 <1999akashpanda@gmail.com>
2026-06-07 20:03:25 +0530

    Date-7 Revising React

--- React-Practice/src/components/Body.jsx
+++ React-Practice/src/components/Body.jsx
@@ -12,7 +12,7 @@ function filterData(searchText, allRestaurants) {
 
   const filterData = allRestaurants.filter((restaurant) => (
     restaurant?.info?.name?.toLowerCase().includes(searchText.toLowerCase())
-  ))
+  ));
   return filterData;
 };
 
